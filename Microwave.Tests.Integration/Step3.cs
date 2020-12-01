281f14e7acea7910d4516d3215091e3f09109b9b
Hold20 <71182715+Hold20@users.noreply.github.com>
2020-12-01 17:18:39 +0100

    Tilføjet step 3 - Mangler stadig

---
+++ Microwave.Tests.Integration/Step3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Tests.Integration
+{
+    class Step3
+    {
+        private IOutput _output;
+        private ILight _light;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = new Output();
+            _light = new Light(_output);
+        }
+
+        [Test]
+        public void TurnOnLight()
+        {
+
+        }
+
+    }
+
+    
+     
+}
